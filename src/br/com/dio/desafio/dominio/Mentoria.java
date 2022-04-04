abcdf4e90cd64f45eb9284694920777bc51cbb0d
Gustavo Siqueira <69218857+siqueira-gustavo@users.noreply.github.com>
2022-04-03 22:19:22 -0300

    Update Mentoria.java

--- src/br/com/dio/desafio/dominio/Mentoria.java
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -24,10 +24,10 @@ public class Mentoria extends Conteudo{
 
     @Override
     public String toString() {
-        return "Mentoria{" +
-                "titulo='" + getTitulo() + '\'' +
-                ", descricao='" + getDescricao() + '\'' +
-                ", data=" + data +
-                '}';
+        return "\n\tMentoria:\n\t{" +
+                "\n\t\ttitulo: '" + getTitulo() + '\'' +
+                ",\n\t\tdescricao: '" + getDescricao() + '\'' +
+                ",\n\t\tdata: " + data +
+                '\n' +'\t'+ "}" + "\n";
     }
 }
